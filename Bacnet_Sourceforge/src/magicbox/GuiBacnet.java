2b33f9e20db25fee1a79eef10fdbd300c72c9bf6
Philip <philip.staberg@gmail.com>
2014-01-07 10:02:54 +0100

    Implementation works

--- Bacnet_Sourceforge/src/magicbox/GuiBacnet.java
+++ Bacnet_Sourceforge/src/magicbox/GuiBacnet.java
@@ -1,4 +1,5 @@
 package magicbox;
+
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
@@ -18,28 +19,27 @@ import javax.swing.Timer;
 import com.serotonin.bacnet4j.exception.BACnetException;
 
 public class GuiBacnet implements ActionListener {
-	
+
 	private JFrame frame;
 	private JTextField newSollH;
 	private JTextField newSollC;
-	private JTextField textFieldH, 	textFieldC;
+	private JTextField textFieldH, textFieldC;
 	private JButton btnExec;
 	private JLabel lblSollwertHeizungs;
 	private JLabel lblSollwertCooling;
 	private JLabel lblActualTemperatures;
-		
+
 	private JCheckBox chckbxFreigabeAnlage;
 	private JCheckBox chckbxHeatingPump;
 	private JCheckBox chckbxCoolingPump;
-	
+	private JCheckBox chckbxManualMode;
+
 	private Timer timer;
-	
-	
-	
+
 	private int tempFahr = 0;
-	
+
 	private BacnetLogic system;
-	
+
 	/**
 	 * Launch the application.
 	 */
@@ -55,12 +55,14 @@ public class GuiBacnet implements ActionListener {
 			}
 		});
 	}
+
 	/**
 	 * Create the application.
 	 */
 	public GuiBacnet() {
 		initialize();
 	}
+
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -68,38 +70,38 @@ public class GuiBacnet implements ActionListener {
 		frame = new JFrame("GUI");
 		timer = new Timer(1000, this);
 		timer.start();
-		
-		//frame.setBounds(100, 100, 450, 300);
-		
+
+		// frame.setBounds(100, 100, 450, 300);
+
 		frame.setMinimumSize(new Dimension(450, 300));
-		
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new GridLayout(5, 3));
 
-		
-		//Actual Temp
+		// Actual Temp
 		lblActualTemperatures = new JLabel("Actual Temperature:");
 
-		//Empty Label
+		// Empty Label
 		JLabel lblEmpty = new JLabel("");
 
-		
-		//Sollwert Heizung
+		// Sollwert Heizung
 		lblSollwertHeizungs = new JLabel("Sollwert Heizung:");
-		
-		//FreigabeAnlage		
+
+		// FreigabeAnlage
 		chckbxFreigabeAnlage = new JCheckBox("Freigabe Anlage: ");
-		
 
 		textFieldH = new JTextField("20", 1);
-	
-		//HeatingPump	
-		chckbxHeatingPump = new JCheckBox("Heating Pumpt: ");
-		
-		//Sollwert Kuhlung
+
+		// Manual mode
+		chckbxManualMode = new JCheckBox("Manual Mode: off");
+
+		// HeatingPump
+		chckbxHeatingPump = new JCheckBox("Heating Pump: ");
+
+		// Sollwert Kuhlung
 		lblSollwertCooling = new JLabel("Sollwert Cooling: ");
-		
-		//CoolingPump	
+
+		// CoolingPump
 		chckbxCoolingPump = new JCheckBox("Cooling Pump: ");
 
 		textFieldC = new JTextField("30", 1);
@@ -107,10 +109,9 @@ public class GuiBacnet implements ActionListener {
 		btnExec = new JButton("Execute");
 		btnExec.addActionListener(this);
 
-		
 		// Add everything to <gridlayout
 		frame.getContentPane().add(lblActualTemperatures);
-		frame.getContentPane().add(lblEmpty);
+		frame.getContentPane().add(chckbxManualMode);
 		frame.getContentPane().add(lblSollwertHeizungs);
 		frame.getContentPane().add(chckbxFreigabeAnlage);
 		frame.getContentPane().add(textFieldH);
@@ -119,7 +120,9 @@ public class GuiBacnet implements ActionListener {
 		frame.getContentPane().add(chckbxCoolingPump);
 		frame.getContentPane().add(textFieldC);
 		frame.getContentPane().add(btnExec);
-		
+
+		chckbxHeatingPump.setEnabled(false);
+		chckbxCoolingPump.setEnabled(false);
 		
 		try {
 			system = new BacnetLogic();
@@ -127,70 +130,110 @@ public class GuiBacnet implements ActionListener {
 		} catch (Exception e) {
 			system.terminate();
 		}
-		
+
 	}
-	
-	
-	
+
 	public void actionPerformed(ActionEvent event) {
-		
+
 		if (event.getSource() == timer) {
 			try {
-				lblSollwertHeizungs.setText(system.readDevice(system.d_sollwertKalten).toString());
-				lblSollwertHeizungs.setText(system.readDevice(system.d_sollwertHeizung).toString());
-				lblActualTemperatures.setText(system.readDevice(system.d_temperature).toString());
-				
-				
-				chckbxFreigabeAnlage.setText("Freigabe Anlage: "+ 
-						system.readDevice(system.d_freigabeAnlagen).toString()=="1"?"on":"off");
-				chckbxHeatingPump.setText("Heating Pump: "+ 
-						system.readDevice(system.d_A06_FRG_Durchlauf).toString()=="1"?"on":"off");
-				chckbxCoolingPump.setText("Cooling Pump: "+ 
-						system.readDevice(system.d_A03_FRG_Kalten).toString()=="1"?"on":"off");
-				
+				lblSollwertCooling
+						.setText("Sollwert Cooling: "
+								+ system.readDevice(system.d_sollwertKalten)
+										.toString());
+				lblSollwertHeizungs.setText("Sollwert Heating: "
+						+ system.readDevice(system.d_sollwertHeizung)
+								.toString());
+				lblActualTemperatures.setText("Actual temperature: "
+						+ system.readDevice(system.d_temperature).toString());
+
+				chckbxFreigabeAnlage.setText("Freigabe Anlage: "
+						+ (system.readDevice(system.d_freigabeAnlagen)
+								.toString().equals("1") ? "on" : "off"));
+				chckbxHeatingPump.setText("Heating Pump: "
+						+ (system.readDevice(system.d_A06_FRG_Durchlauf)
+								.toString().equals("1") ? "on" : "off"));
+				chckbxCoolingPump.setText("Cooling Pump: "
+						+ (system.readDevice(system.d_A03_FRG_Kalten)
+								.toString().equals("1") ? "on" : "off"));
+
 			} catch (BACnetException e) {
 				e.printStackTrace();
 			}
-			
-		}
-		else if (event.getSource() == btnExec) {
-			
-			System.out.println("Excecute");
-			System.out.println(chckbxCoolingPump.isSelected());
-			
+
+		} else if (event.getSource() == btnExec) {
+
 			try {
-				system.writeDevice(system.d_A03_FRG_Kalten ,chckbxCoolingPump.isSelected());
-				system.writeDevice(system.d_A06_FRG_Durchlauf ,chckbxHeatingPump.isSelected());
-				system.writeDevice(system.d_freigabeAnlagen ,chckbxFreigabeAnlage.isSelected());
-				
-				if (!textFieldH.getText().isEmpty() && isNumeric(textFieldH.getText())) {
+				system.writeDevice(system.d_freigabeAnlagen,
+						chckbxFreigabeAnlage.isSelected());
+				chckbxManualMode.setText("Manual Mode: "
+						+ (chckbxManualMode.isSelected()? "on" : "off"));
+				if (chckbxManualMode.isSelected()) {
 
-					system.writeDevice(system.d_sollwertHeizung, (int)((Double.parseDouble(textFieldH.getText()))));
-				}
-				if (!textFieldH.getText().isEmpty() && isNumeric(textFieldH.getText())) {
+					chckbxCoolingPump.setEnabled(true);
+					chckbxHeatingPump.setEnabled(true);
+					textFieldC.setEnabled(false);
+					textFieldH.setEnabled(false);
+					system.writeDevice(system.d_A03_FRG_Kalten,
+							chckbxCoolingPump.isSelected());
+					system.writeDevice(system.d_A06_FRG_Durchlauf,
+							chckbxHeatingPump.isSelected());
+					
+					if (chckbxCoolingPump.isSelected()){
+						system.writeDevice(system.d_M02_VG, 100);
+						system.writeDevice(system.d_M04_VG, 100);
+						system.writeDevice(system.d_M05_VG, 100);
+					}
+					else {
+						system.writeDevice(system.d_M02_VG, 0);
+					}
+					if (chckbxHeatingPump.isSelected()){
+						system.writeDevice(system.d_M03_VG, 100);
+						system.writeDevice(system.d_M04_VG, 100);
+						system.writeDevice(system.d_M05_VG, 100);
+					}
+					else {
+						system.writeDevice(system.d_M03_VG, 0);
+					}
+					if (chckbxCoolingPump.isSelected() && chckbxCoolingPump.isSelected()){
+						system.writeDevice(system.d_M04_VG, 0);
+						system.writeDevice(system.d_M05_VG, 0);
+					}
+					
+				} else {
+					textFieldC.setEnabled(true);
+					textFieldH.setEnabled(true);
+					chckbxCoolingPump.setEnabled(false);
+					chckbxHeatingPump.setEnabled(false);
 
-					system.writeDevice(system.d_sollwertKalten, (int)((Double.parseDouble(textFieldC.getText()))));
+					if (!textFieldH.getText().isEmpty()
+							&& isNumeric(textFieldH.getText())) {
+
+						system.writeDevice(
+								system.d_sollwertHeizung,
+								(int) ((Double.parseDouble(textFieldH.getText()))));
+					}
+					if (!textFieldH.getText().isEmpty()
+							&& isNumeric(textFieldH.getText())) {
+
+						system.writeDevice(
+								system.d_sollwertKalten,
+								(int) ((Double.parseDouble(textFieldC.getText()))));
+					}
 				}
-				
-			} catch (BACnetException e) {
-				e.printStackTrace();
+			} catch (BACnetException e1) {
+				e1.printStackTrace();
 			}
 		}
-    }
-	
-	public static boolean isNumeric(String str)  
-	{  
-	  try  
-	  {  
-	    double d = Double.parseDouble(str);  
-	  }  
-	  catch(NumberFormatException nfe)  
-	  {  
-	    return false;  
-	  }  
-	  return true;  
 	}
-	
+
+	public static boolean isNumeric(String str) {
+		try {
+			double d = Double.parseDouble(str);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+		return true;
+	}
+
 }
-
-
